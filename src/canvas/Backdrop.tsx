a15121fa9a54694f196adf6686576401157d5512
Sam <samuel.fitz@btinternet.com>
2023-06-11 16:23:56 +0100

    feat: add canvas

---
+++ src/canvas/Backdrop.tsx
@@ -0,0 +1,14 @@
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+
+const Backdrop = () => {
+  return (
+    <AccumulativeShadows position={[0, 0, -0.14]}>
+      <RandomizedLight amount={4} />
+    </AccumulativeShadows>
+  );
+};
+
+export default Backdrop;
