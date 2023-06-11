f2713d661a073b3030054fb953cc1b79f568f0b0
Sam <samuel.fitz@btinternet.com>
2023-06-11 14:22:16 +0100

    feat: add editor features

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 import Button from "./Button";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export { Button };
+export { Button, AIPicker, ColorPicker, FilePicker, Tab };
--- src/pages/Customiser.tsx
+++ src/pages/Customiser.tsx
@@ -1,5 +1,63 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants.js";
+import { fadeAnimation, slideAnimation } from "../config/motion.js";
+
+import { AIPicker, ColorPicker, FilePicker, Button, Tab } from "../components";
+
 const Customiser = () => {
-  return <div>Customiser</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="customiser"
+            {...slideAnimation("left")}
+          >
+            <div className="tab-wrapper tabs glassmorphism">
+              <div className="tab">
+                {EditorTabs.map((tab: any) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div className="button-wrapper">
+            <Button
+              type="filled"
+              title="Go Back"
+              handleClick={() => (state.intro = true)}
+              customClass="back-button"
+            ></Button>
+          </motion.div>
+
+          <motion.div
+            className="filter-tabs-container"
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab: any) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=""
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customiser;
